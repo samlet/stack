362a8c6599cca0571d3712016b594750c6a3684b
Samlet Wu <xiaofei.wu@gmail.com>
2019-12-31 05:51:28 +0800

    fix version

--- sagas/sagas_cli.py
+++ sagas/sagas_cli.py
@@ -1,4 +1,4 @@
 class SagasCli(object):
     def version(self):
         from sagas.version import __version__
-        print(f"saai version: {__version__}")
+        print(f"sagas version: {__version__}")
