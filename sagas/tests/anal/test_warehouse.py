c04198236ccacbe1d9e6034c80c01326a3da05c2
Samlet Wu <xiaofei.wu@gmail.com>
2020-04-23 18:30:15 +0800

    fix unittest

--- sagas/tests/anal/test_warehouse.py
+++ sagas/tests/anal/test_warehouse.py
@@ -5,7 +5,7 @@ def test_gid():
     from sagas.nlu.warehouse import Warehouse
     wh = Warehouse.create()
     gid = 'UHJvZHVjdFR5cGU6QUdHUkVHQVRFRA=='
-    val = wh.resolve_entity(gid)
+    val = wh.resolve_entity(gid).value
     assert 'ProductType'==val.getEntityName()
     assert gid==wh.get_gid(val)
 
