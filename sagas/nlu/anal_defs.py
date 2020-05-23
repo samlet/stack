1917fb1598e4d29e04f98b3533c960313005e010
Samlet Wu <xiaofei.wu@gmail.com>
2020-05-24 02:31:30 +0800

    add anal_ql features: annotations, schema files

--- sagas/nlu/anal_defs.py
+++ sagas/nlu/anal_defs.py
@@ -1,2 +1,17 @@
 terms_list=['ent', 'srv', 'fld', 'ref', 'typ',
             'loc']
+
+def make_registrar():
+    registry = {}
+    def registrar(func):
+        fname=func.__name__
+        if fname.startswith('_'):
+            fname=fname[1:]
+        registry[fname] = func
+        return func  # normally a decorator returns a wrapped function,
+                     # but here we return func unmodified, after registering it
+    registrar.all = registry
+    return registrar
+
+vtable = make_registrar()
+
--- sagas/nlu/anal_ql.py
+++ sagas/nlu/anal_ql.py
@@ -2,53 +2,14 @@ from graphql import graphql_sync
 from ariadne import ObjectType, QueryType, make_executable_schema
 from ariadne import make_executable_schema, load_schema_from_path, ObjectType, QueryType
 import json
+
+from sagas.nlu.anal_defs import vtable
 from sagas.util.collection_util import wrap, to_obj
 from sagas.nlu.utils import fix_sents
 import pandas as pd
 from sagas.nlu.anal import delegator
 from sagas.nlu.anal_expr import match
 from sagas.nlu.anal_data_types import behave_, desc_, phrase_, rel_, path_, _, _1, _2
-
-type_defs = """
-type Query {
-    behave_with_id(_id: ID!): Behave
-    behave_with_verb(_verb: String!): Behave
-    desc_with_id(_id: ID!): Desc
-    bucket_behaves(_id: ID!): Bucket
-}
-
-type Modifier{
-    part: String!
-    cont: String!
-}
-type Behave {
-    id: ID!
-    subj: String!
-    obj: String!
-    iobj: String!
-    behave: String!
-    behave_spec: String!
-    obj_spec: String!
-}
-type Desc {
-    id: ID!
-    subj: String!
-    aux: String!
-    desc: String!
-    modifiers: [Modifier]
-}
-type Phrase {
-    id: ID!
-    head: String!
-    modifiers: [Modifier]
-}
-type Bucket{
-    id: ID!
-    behaves: [Behave]
-    descs: [Desc]
-    phrases: [Phrase]
-}
-"""
 
 def get_corpus(lang, chapter):
     dfjson = pd.read_json(f'~/pi/stack/crawlers/langcrs/all_{lang}.json')
@@ -79,6 +40,7 @@ def resolve_behaves(bucket, info):
                 results.append(wrap_behave(f_behave))
     return results
 
+@vtable
 def _study(f, f_behave):
     return match(f,
                  behave_(_, _1 << 'study|学习', _2 << _, _), lambda arg, *v: wrap_behave(f_behave),
@@ -86,10 +48,30 @@ def _study(f, f_behave):
                  _, None)
 
 def bucket_behaves(x, info, _id):
-    mappings={'study': _study}
+    # mappings={'study': _study}
+    mappings=vtable.all
     return wrap(id=_id,
                 routine=mappings[_id],
                 rs=get_corpus('pt', 'At school')) if _id in mappings else None
+
+
+class AnalQl(object):
+    def __init__(self):
+        from sagas.conf import resource_path
+        self.type_defs = load_schema_from_path(resource_path('schemas.graphql'))
+        query = QueryType()
+        bucket = ObjectType('Bucket')
+        behave = ObjectType('Behave')
+        desc = ObjectType('Desc')
+
+        query.set_field('bucket_behaves', bucket_behaves)
+        bucket.set_field('behaves', resolve_behaves)
+        self.schema = make_executable_schema(self.type_defs, [behave, bucket, query])
+
+    def query_sync(self, q):
+        return graphql_sync(self.schema, q)
+
+anal_ql=AnalQl()
 
 class AnalQlProcs(object):
     def bucket(self, id='study'):
@@ -107,16 +89,6 @@ class AnalQlProcs(object):
         :return:
         """
         from pprint import pprint
-
-        query = QueryType()
-        bucket = ObjectType('Bucket')
-        behave = ObjectType('Behave')
-        desc = ObjectType('Desc')
-
-        query.set_field('bucket_behaves', bucket_behaves)
-        bucket.set_field('behaves', resolve_behaves)
-        schema = make_executable_schema(type_defs, [behave, bucket, query])
-
         q = """{
             bucket_behaves(_id:"study"){     
                 id
@@ -128,7 +100,7 @@ class AnalQlProcs(object):
             }
         }
         """
-        result = graphql_sync(schema, q)
+        result = anal_ql.query_sync(q)
         if not result.errors:
             pprint(result.data)
         else:
